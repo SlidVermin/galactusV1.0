ddaed1f3280ae20f643a230c82c8fa54888d93e5
Poseidon <42254061+jhonatan1043@users.noreply.github.com>
2019-02-22 11:32:20 -0500

    terminando diseño del formulario de informe quirurgico

---
+++ Entidades/HistoriaClinica/Resultado/InformeQuirurgico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galactus.Entidades.HistoriaClinica.Resultado
+{
+    class InformeQuirurgico
+    {
+        public string codigo { get; set; }
+        public int codigoAtencion { get; set; }
+        public int codigoVia {get; set;} 
+        public int codigoAyudante { get; set; }
+        public int codigoAnastesiologo { get; set; }
+        public int codigoAnastesia { get; set; }
+        public string notaQuirurgica { get; set; }
+
+        public InformeQuirurgico() {
+
+        }
+
+    }
+}
