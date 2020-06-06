3bf2052373459af535120390b23f0ddb3cb8d1ab
Unknown <unknown@example.com>
2020-06-06 11:06:59 -0500

    Limpiando repositorio

---
+++ VistaControlador/LoginUI.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galactus
+{
+    public partial class InicioUI : Form
+    {
+        public InicioUI()
+        {
+            InitializeComponent();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+            pnlLogin.BackColor = Color.FromArgb(200, 107, 107, 107);
+            lblUsuario.BackColor = Color.Transparent;
+            lblContrasena.BackColor = Color.Transparent;
+            lbSede.BackColor = Color.Transparent;
+            btIngresar.BackColor = Color.FromArgb(180, 131, 173, 210);
+            btSalir.BackColor = Color.FromArgb(180, 131, 173, 210);
+        }
+
+        void iniciarSesion(string usuario, string pass)
+        {
+            try
+            {
+                List<string> list = new List<string>();
+                list.Add(usuario);
+                list.Add(pass);
+                if (iniciarSesion(list))
+                {
+                    PrincipalUI principalUI = new PrincipalUI();
+                    principalUI.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + ex.Message);
+            }
+        }
+
+        public bool iniciarSesion(List<string> listaParametros)
+        {
+            DataRow filaResultado = null;
+            filaResultado = GeneralC.devuelveUnaFila(Sentencias.SEG_AUTENTICACION, listaParametros);
+            if (filaResultado != null)
+            {
+                Sesion.IdRol = (int)filaResultado.ItemArray[0];
+                Sesion.IdUsuario = (int)filaResultado.ItemArray[1];
+                Sesion.nombre = filaResultado.Field<string>("Nombre");
+                return true;
+            }
+            return false;
+        }
+
+        private void btIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "" || txtContrasena.Text == "")
+            {
+                MessageBox.Show("los campos marcados son obligatorios", "", MessageBoxButtons.OK);
+            }
+            else
+            {
+                iniciarSesion(txtUsuario.Text, txtContrasena.Text);
+            }
+        }
+
+        private void btSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ VistaControlador/LoginUI.designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Galactus
+{
+    partial class InicioUI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLogin = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btSalir = new System.Windows.Forms.Button();
+            this.btIngresar = new System.Windows.Forms.Button();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.lbSede = new System.Windows.Forms.Label();
+            this.lblContrasena = new System.Windows.Forms.Label();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.pnlLogin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlLogin
+            // 
+            this.pnlLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(107)))), ((int)(((byte)(107)))), ((int)(((byte)(107)))));
+            this.pnlLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlLogin.Controls.Add(this.comboBox1);
+            this.pnlLogin.Controls.Add(this.btSalir);
+            this.pnlLogin.Controls.Add(this.btIngresar);
+            this.pnlLogin.Controls.Add(this.txtContrasena);
+            this.pnlLogin.Controls.Add(this.txtUsuario);
+            this.pnlLogin.Controls.Add(this.lbSede);
+            this.pnlLogin.Controls.Add(this.lblContrasena);
+            this.pnlLogin.Controls.Add(this.lblUsuario);
+            this.pnlLogin.Location = new System.Drawing.Point(170, 81);
+            this.pnlLogin.Name = "pnlLogin";
+            this.pnlLogin.Size = new System.Drawing.Size(432, 242);
+            this.pnlLogin.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(136, 150);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(262, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // btSalir
+            // 
+            this.btSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.btSalir.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btSalir.FlatAppearance.BorderSize = 0;
+            this.btSalir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btSalir.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSalir.ForeColor = System.Drawing.Color.White;
+            this.btSalir.Location = new System.Drawing.Point(295, 189);
+            this.btSalir.Name = "btSalir";
+            this.btSalir.Size = new System.Drawing.Size(103, 34);
+            this.btSalir.TabIndex = 9;
+            this.btSalir.Text = "Salir";
+            this.btSalir.UseVisualStyleBackColor = false;
+            this.btSalir.Click += new System.EventHandler(this.btSalir_Click);
+            // 
+            // btIngresar
+            // 
+            this.btIngresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(131)))), ((int)(((byte)(173)))), ((int)(((byte)(210)))));
+            this.btIngresar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btIngresar.FlatAppearance.BorderSize = 0;
+            this.btIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btIngresar.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btIngresar.ForeColor = System.Drawing.Color.White;
+            this.btIngresar.Location = new System.Drawing.Point(154, 189);
+            this.btIngresar.Name = "btIngresar";
+            this.btIngresar.Size = new System.Drawing.Size(103, 34);
+            this.btIngresar.TabIndex = 8;
+            this.btIngresar.Text = "Ingresar";
+            this.btIngresar.UseVisualStyleBackColor = false;
+            this.btIngresar.Click += new System.EventHandler(this.btIngresar_Click);
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(136, 118);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(262, 20);
+            this.txtContrasena.TabIndex = 6;
+            this.txtContrasena.UseSystemPasswordChar = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(136, 85);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(262, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // lbSede
+            // 
+            this.lbSede.AutoSize = true;
+            this.lbSede.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSede.ForeColor = System.Drawing.Color.White;
+            this.lbSede.Location = new System.Drawing.Point(32, 150);
+            this.lbSede.Name = "lbSede";
+            this.lbSede.Size = new System.Drawing.Size(41, 16);
+            this.lbSede.TabIndex = 3;
+            this.lbSede.Text = "Sede";
+            // 
+            // lblContrasena
+            // 
+            this.lblContrasena.AutoSize = true;
+            this.lblContrasena.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContrasena.ForeColor = System.Drawing.Color.White;
+            this.lblContrasena.Location = new System.Drawing.Point(32, 117);
+            this.lblContrasena.Name = "lblContrasena";
+            this.lblContrasena.Size = new System.Drawing.Size(80, 16);
+            this.lblContrasena.TabIndex = 2;
+            this.lblContrasena.Text = "Contraseña";
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.Color.White;
+            this.lblUsuario.Location = new System.Drawing.Point(32, 85);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(56, 16);
+            this.lblUsuario.TabIndex = 1;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Galactus.Properties.Resources.login_fondo;
+            this.ClientSize = new System.Drawing.Size(752, 386);
+            this.Controls.Add(this.pnlLogin);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.pnlLogin.ResumeLayout(false);
+            this.pnlLogin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogin;
+        private System.Windows.Forms.Label lbSede;
+        private System.Windows.Forms.Label lblContrasena;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.TextBox txtContrasena;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Button btIngresar;
+        private System.Windows.Forms.Button btSalir;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
